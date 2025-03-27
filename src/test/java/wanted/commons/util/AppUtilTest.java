4e512c9da900ce9357dec793cbd2fbafb54be7c7
ongxinyii <e1278125@u.nus.edu>
2025-03-27 17:38:20 +0800

    Change format of GUI

--- src/test/java/wanted/commons/util/AppUtilTest.java
+++ src/test/java/wanted/commons/util/AppUtilTest.java
@@ -9,7 +9,7 @@ public class AppUtilTest {
 
     @Test
     public void getImage_exitingImage() {
-        assertNotNull(AppUtil.getImage("/images/ledger.png"));
+        assertNotNull(AppUtil.getImage("/images/WantedList.png"));
     }
 
     @Test
