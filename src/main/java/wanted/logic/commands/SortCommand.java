c66353089da9f53edb175448ed765b242be4a42a
nikingoda <ngoducanh6a01@gmail.com>
2025-03-27 19:07:11 +0800

    Add sort command

---
+++ src/main/java/wanted/logic/commands/SortCommand.java
@@ -0,0 +1,33 @@
+package wanted.logic.commands;
+
+import javafx.collections.ObservableList;
+import wanted.logic.commands.exceptions.CommandException;
+import wanted.model.LoanBook;
+import wanted.model.Model;
+import wanted.model.loan.Loan;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Sort loan by loan amount value. Maybe have more options in future
+ */
+public class SortCommand  extends Command{
+    public static final String COMMAND_WORD = "sort";
+    public static final String MESSAGE_SUCCESS = "Sort success";
+    public static final String MESSAGE_FAIL = "Sort failed";
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        try {
+            LoanBook loanBook = (LoanBook) model.getAddressBook();
+            ObservableList<Loan> oldList = loanBook.getPersonList();
+            Comparator<Loan> comparator =
+                    Comparator.nullsLast(Comparator.comparingInt(a -> - a.getAmount().value.getValueTimesOneHundred()));
+            List<Loan> sortedList = oldList.sorted(comparator);
+            loanBook.setPersons(sortedList);
+        } catch (Exception e) {
+            throw new CommandException(MESSAGE_FAIL + ": " + e.getMessage());
+        }
+        return new CommandResult(String.format(MESSAGE_SUCCESS));
+    }
+}
