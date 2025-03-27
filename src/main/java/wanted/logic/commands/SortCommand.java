10c535cda384d15e56ce8237a934661260c39183
nikingoda <ngoducanh6a01@gmail.com>
2025-03-27 19:48:07 +0800

    Remove unnecessary try-catch block

--- src/main/java/wanted/logic/commands/SortCommand.java
+++ src/main/java/wanted/logic/commands/SortCommand.java
@@ -15,21 +15,16 @@ import wanted.model.loan.Loan;
 public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
     public static final String MESSAGE_SUCCESS = "Sort success";
-    public static final String MESSAGE_FAIL = "Sort failed";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        try {
-            LoanBook loanBook = (LoanBook) model.getAddressBook();
-            ObservableList<Loan> oldList = loanBook.getPersonList();
-            Comparator<Loan> comparator =
-                    Comparator.nullsLast(Comparator.comparingInt(a -> (
-                            -a.getAmount().remainingValue.getValueTimesOneHundred())));
-            List<Loan> sortedList = oldList.sorted(comparator);
-            loanBook.setPersons(sortedList);
-        } catch (Exception e) {
-            throw new CommandException(MESSAGE_FAIL + ": " + e.getMessage());
-        }
+        LoanBook loanBook = (LoanBook) model.getAddressBook();
+        ObservableList<Loan> oldList = loanBook.getPersonList();
+        Comparator<Loan> comparator =
+                Comparator.nullsLast(Comparator.comparingInt(a -> (
+                        -a.getAmount().remainingValue.getValueTimesOneHundred())));
+        List<Loan> sortedList = oldList.sorted(comparator);
+        loanBook.setPersons(sortedList);
         return new CommandResult(String.format(MESSAGE_SUCCESS));
     }
 }
