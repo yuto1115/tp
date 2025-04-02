e162ab2e4d2c4c5407fb0724a6a7229723228880
sunpterodactyl <inforsunpteranadon@gmail.com>
2025-04-03 02:59:01 +0800

    Introduce retag command

---
+++ src/main/java/wanted/logic/parser/RetagCommandParser.java
@@ -0,0 +1,67 @@
+package wanted.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static wanted.logic.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+import static wanted.logic.parser.CliSyntax.PREFIX_NAME;
+import static wanted.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+
+import wanted.commons.core.datatypes.Index;
+import wanted.logic.commands.BaseEdit;
+import wanted.logic.commands.EditCommand;
+import wanted.logic.commands.RetagCommand;
+import wanted.logic.parser.exceptions.ParseException;
+import wanted.model.tag.Tag;
+
+/**
+ * Parser to parse repay command
+ */
+public class RetagCommandParser {
+    /**
+     * Javadoc
+     */
+    public RetagCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_TAG);
+
+        Index index;
+        try {
+            index = index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_PERSON_DISPLAYED_INDEX, RetagCommand.MESSAGE_USAGE), pe);
+        }
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_TAG);
+        BaseEdit.EditLoanDescriptor editPersonDescriptor = new BaseEdit.EditLoanDescriptor();
+
+        if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
+            parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editPersonDescriptor::setTags);
+        }
+
+        if (!editPersonDescriptor.isAnyFieldEdited()) {
+            throw new ParseException(EditCommand.MESSAGE_NOT_EDITED); //change this later
+        }
+
+        return new RetagCommand(index, editPersonDescriptor);
+    }
+
+    /**
+     * Parses {@code Collection<String> tags} into a {@code Set<Tag>} if {@code tags} is non-empty.
+     * If {@code tags} contain only one element which is an empty string, it will be parsed into a
+     * {@code Set<Tag>} containing zero tags.
+     */
+    private Optional<Set<Tag>> parseTagsForEdit(Collection<String> tags) throws ParseException {
+        assert tags != null;
+
+        if (tags.isEmpty()) {
+            return Optional.empty();
+        }
+        Collection<String> tagSet = tags.size() == 1 && tags.contains("") ? Collections.emptySet() : tags;
+        return Optional.of(ParserUtil.parseTags(tagSet));
+    }
+}
