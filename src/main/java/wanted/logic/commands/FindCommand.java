0153174fe229ab2c16471669286875e2d1dcebf9
ongxinyii <e1278125@u.nus.edu>
2025-04-03 23:27:20 +0800

    Fix phrasing

--- src/main/java/wanted/logic/commands/FindCommand.java
+++ src/main/java/wanted/logic/commands/FindCommand.java
@@ -28,8 +28,7 @@ public class FindCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Finds all persons whose names contain any specified keywords (case-insensitive).\n"
-            + "All results that match the keywords will be listed in alphabetical order\n"
-            + "\n"
+            + "Results that match any/all keywords will be listed in alphabetical order\n"
             + "Parameters:\n"
             + "    [KEYWORD]\n"
             + "    ([MORE KEYWORDS]...)\n"
