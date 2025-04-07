140714fa574fdebaa4be33c9b2164de3bb3beb94
nikingoda <ngoducanh6a01@gmail.com>
2025-04-07 17:15:17 +0800

    Update ListCommand.java

--- src/main/java/wanted/logic/commands/ListCommand.java
+++ src/main/java/wanted/logic/commands/ListCommand.java
@@ -1,9 +1,14 @@
 package wanted.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static wanted.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import javafx.collections.ObservableList;
+import wanted.model.LoanBook;
 import wanted.model.Model;
+import wanted.model.loan.Loan;
+
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Lists all persons in the loan book to the user.
@@ -18,7 +23,12 @@ public class ListCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        LoanBook loanBook = (LoanBook) model.getLoanBook();
+        ObservableList<Loan> oldList = loanBook.getPersonList();
+        Comparator<Loan> comparator =
+                Comparator.comparing(l -> l.getName().toString().toLowerCase());
+        List<Loan> sortedList = oldList.sorted(comparator);
+        loanBook.setPersons(sortedList);
+        return new CommandResult(String.format(MESSAGE_SUCCESS));
     }
 }
